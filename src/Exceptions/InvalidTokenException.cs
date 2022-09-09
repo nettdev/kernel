065df9b5b461e025a521b6f95e9001a42646563b
Jeziel Moura <jezielmoura@outlook.com>
2022-09-08 21:01:42 -0300

    Add more tests

--- src/Exceptions/InvalidTokenException.cs
+++ src/Exceptions/InvalidTokenException.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Mobnet.SharedKernel;
 
+[ExcludeFromCodeCoverage]
 public class InvalidTokenException : DomainException
 {
     public InvalidTokenException() : base("O Token informado é inválido")
