15bc4dc87b6f09543663a7620cd7c1f342b917cb
Jeziel Moura <jezielmoura@outlook.com>
2022-09-09 21:04:11 -0300

    Add resource visibility

--- src/Attributes/AuthorizeAttribute.cs
+++ src/Attributes/AuthorizeAttribute.cs
@@ -3,14 +3,16 @@ namespace Mobnet.SharedKernel;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
 public class AuthorizeAttribute : Attribute
 {
-    public AuthorizeAttribute(string permission, string group, string label) 
+    public string Permission { get; }
+    public string Group { get; }
+    public string Label { get; }
+    public bool VisibleInResourceList { get; }
+
+    public AuthorizeAttribute(string permission, string group, string label, bool visibleInResourceList = true) 
     {
         Permission = permission;
         Group = group;
         Label = label;
+        VisibleInResourceList = visibleInResourceList;
     }
-
-    public string Permission { get; }
-    public string Group { get; }
-    public string Label { get; }
 }
