2603429a74dbeccaa0c764f81b0ef1f9ea0c24f8
Jeziel Moura <jezielmoura@outlook.com>
2022-11-15 11:12:33 -0300

    Remove powershell script

--- release.ps1
+++
@@ -1,7 +0,0 @@
-$regex = "<PackageVersion>[0-9]{1,5}.[0-9]{1,5}.[0-9]{1,5}<\/PackageVersion>"
-$version=$args[0]
-$packageVersion = "<PackageVersion>$($version)</PackageVersion>"
-
-$presentation = (Get-Content "./src/Kernel.csproj") -replace $regex, $packageVersion
-
-$presentation | Set-Content "./src/Kernel.csproj"
